8be61d6b7030c6c9a62a3092df0de1593fef1e46
Charles Wilson <charles.wilson@umontreal.ca>
2021-10-01 22:44:49 -0400

    initial commit

---
+++ library_manager.py
@@ -0,0 +1,30 @@
+"""
+This script allows for manipulation of the album_list file
+"""
+
+import numpy as np
+import re
+
+library = open("./album_list.txt", "r")
+
+# unpack lines
+
+album = []
+artist = []
+genre = []
+
+for i,l in enumerate(library):
+    if l[0] == "#": continue
+
+
+    ll = re.split('\t+', l)
+
+    if i == 32:
+        print(ll)
+        break
+
+    album.append(ll[0].strip('"\n'))
+    artist.append(ll[1].strip('"\n'))
+    genre.append((ll[2]).strip('"\n'))
+
+    # print("halte")
